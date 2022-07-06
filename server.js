567418d7907732e50bc8f8a35cd10a209881bfb6
Vicente <vicentecrespo45@gmail.com>
2022-07-06 15:03:52 -0300

    frontend

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const DB_USER = process.env.DB_USER
 const DB_PASS = process.env.DB_PASS
 const url = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.liky5cy.mongodb.net/?retryWrites=true&w=majority`
 
-const port = env.process.PORT || 3000
+const port = process.env.PORT || 3000
 
 mongoose.connect(url)
     .then(() => {
