3379650755e85c8c96794bc936d93221a74f2f49
Vicente <vicentecrespo45@gmail.com>
2022-07-07 08:28:32 -0300

    backend

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json())
 
 // importar rotas
-app.route('/', (req, res) => {
+app.use('/', (req, res) => {
     res.status(200).json({ msg: "API funcionando!" })
 })
 
