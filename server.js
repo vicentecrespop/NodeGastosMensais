f846e26657c23dcb20c4f488e99ccee595ee16a3
vicentecrespop <vicentecrespo45@gmail.com>
2022-08-16 16:28:24 -0300

    node

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json())
 
 // importar rotas
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
     res.status(200).json({ msg: "API funcionando!" })
 })
 
